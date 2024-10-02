4b55ab5b24378a5c23cfc93261063857cd42f40b
Jacob Roman <jcbrmn06@gmail.com>
2024-10-01 20:56:08 -0400

    inital commit

---
+++ tailwind.config.ts
@@ -0,0 +1,112 @@
+import headlessuiPlugin from '@headlessui/tailwindcss'
+import formsPlugin from '@tailwindcss/forms'
+import { type Config } from 'tailwindcss'
+
+export default {
+    darkMode: ['class'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+  	fontSize: {
+  		xs: ['0.75rem', { lineHeight: '1rem' }],
+  		sm: ['0.875rem', { lineHeight: '1.5rem' }],
+  		base: ['1rem', { lineHeight: '1.5rem' }],
+  		lg: ['1.125rem', { lineHeight: '2rem' }],
+  		xl: ['1.25rem', { lineHeight: '1.75rem' }],
+  		'2xl': ['1.5rem', { lineHeight: '2rem' }],
+  		'3xl': ['2rem', { lineHeight: '3rem' }],
+  		'4xl': ['2.5rem', { lineHeight: '3rem' }],
+  		'5xl': ['3rem', { lineHeight: '1' }],
+  		'6xl': ['3.75rem', { lineHeight: '1' }],
+  		'7xl': ['4.5rem', { lineHeight: '1' }],
+  		'8xl': ['6rem', { lineHeight: '1' }],
+  		'9xl': ['8rem', { lineHeight: '1' }]
+  	},
+  	extend: {
+  		animation: {
+  			'fade-in': 'fade-in 0.5s linear forwards',
+  			marquee: 'marquee var(--marquee-duration) linear infinite',
+  			'spin-slow': 'spin 4s linear infinite',
+  			'spin-slower': 'spin 6s linear infinite',
+  			'spin-reverse': 'spin-reverse 1s linear infinite',
+  			'spin-reverse-slow': 'spin-reverse 4s linear infinite',
+  			'spin-reverse-slower': 'spin-reverse 6s linear infinite'
+  		},
+  		borderRadius: {
+  			'4xl': '2rem',
+  			'5xl': '2.5rem',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: 'var(--font-inter)'
+  		},
+  		keyframes: {
+  			'fade-in': {
+  				from: {
+  					opacity: '0'
+  				},
+  				to: {
+  					opacity: '1'
+  				}
+  			},
+  			marquee: {
+  				'100%': {
+  					transform: 'translateY(-50%)'
+  				}
+  			},
+  			'spin-reverse': {
+  				to: {
+  					transform: 'rotate(-360deg)'
+  				}
+  			}
+  		},
+  		maxWidth: {
+  			'2xl': '40rem'
+  		}
+  	}
+  },
+  plugins: [formsPlugin, headlessuiPlugin, require("tailwindcss-animate")],
+} satisfies Config
